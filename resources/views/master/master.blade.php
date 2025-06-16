77e07385c530728ad7fa7c4ce2558ff356db553c
Syamil Maranri <symlmrnri17@gmail.com>
2025-06-16 09:40:37 +0700

    Update department test

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -5,7 +5,7 @@
 
     <div class="d-flex gap-4 mt-3 w-100">
         <div class="body-content scrollable-container rounded-4 p-5 w-50 d-flex justify-content-center align-items-center" style="height: 200px;">
-            <a href="/department" class="text-decoration-none text-dark d-flex flex-column align-items-center">
+<a href="{{ url('department') }}" class="text-decoration-none text-dark d-flex flex-column align-items-center">
             
                 <h5>Department</h5>
             </a>
