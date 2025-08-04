804330223ff1252a14c01d8a65cb52e0de182c20
Syamil Maranri <symlmrnri17@gmail.com>
2025-08-04 17:23:19 +0700

    Updat styling btn submit black in all page

--- public/asset/js/office.js
+++ public/asset/js/office.js
@@ -140,7 +140,6 @@ $(document).ready(function() {
                     </div>
                 `;
             });
-            
             notificationList.html(html);
         });
             },
