2aa75a52d2521bb0c688d5940239cb2e6c424690
Fahrel Rhozian <fahrelrhozian09@gmail.com>
2025-08-13 13:33:30 +0700

    testing branch

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,10 @@
         <h2>Dashboard</h2>
     </div>
     <div class="body-content scrollable-container rounded-4 p-5" style="">
-       
+
     </div>
     </div>
     <script>
     </script>
-
+ 
 </x-office-layout>
