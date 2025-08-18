0dd7f0b2992cc30e39cd79777a0476ae26b517e3
Syamil Maranri <symlmrnri17@gmail.com>
2025-08-18 21:27:41 +0700

    Update storaging device field

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -412,7 +412,7 @@ if ($request->hasFile('image')) {
                     $attendanceTracking->update([
                         'type' => 'check_out',
                         'image' => $mergedTrackingImages,
-                        'device' => $checkInDevice . '|' . $checkOutDevice, // Gabungkan dengan separator |
+                        'device' => $checkInDevice . ' , ' . $checkOutDevice, // Gabungkan dengan separator |
                         'date_time' => $now,
                         'updated_by' => $userId,
                     ]);
