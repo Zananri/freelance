4eb13c291866cd07eb7043feffb818055e87f8a8
Syamil Maranri <symlmrnri17@gmail.com>
2025-08-20 11:00:31 +0700

    Update fix styling layouting modal check in for attendance and dashboard

--- resources/views/attendance/attendance.blade.php
+++ resources/views/attendance/attendance.blade.php
@@ -203,7 +203,7 @@
                 <!-- Modal Footer -->
                 <div class="modal-footer modal-footer-custom">
                     <button type="submit" class="btn btn-submit-black" id="submitCheckInBtn">
-                        <span class="material-symbols-outlined">alarm_on</span> Check In
+                        Check In
                     </button>
                 </div>
 
