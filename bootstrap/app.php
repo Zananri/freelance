cbaab773a85b953ab21f0b91dba422193c981387
Syamil Maranri <symlmrnri17@gmail.com>
2025-09-15 18:33:31 +0700

    Update schedule run

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,9 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
-use App\Console\Commands\GenerateTasksFromSchedules;
 use App\Http\Middleware\Management;
-use App\Console\Commands\DiagnoseSchedules;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,10 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withCommands([
-        GenerateTasksFromSchedules::class,
-        DiagnoseSchedules::class,
-    ])
     ->withSchedule(function (Schedule $schedule) {
         // Run schedule generators by recurrence type at sensible intervals
         $schedule->command('schedules:generate --type=daily')->dailyAt('00:05')->onOneServer();
