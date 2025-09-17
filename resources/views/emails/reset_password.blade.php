093be7f7751deabfee709b204ac7c029c6620203
Syamil Maranri <symlmrnri17@gmail.com>
2025-09-17 11:52:30 +0700

    Update function reset password v4 styling email

--- resources/views/emails/reset_password.blade.php
+++ resources/views/emails/reset_password.blade.php
@@ -21,7 +21,7 @@
       <h3>Hello!</h3>
       <p class="muted">You are receiving this email because we received a password reset request for your account.</p>
 
-      <p style="text-align:center;margin:30px 0"><a href="{{ $url }}" class="button">Reset Password</a></p>
+      <p style="text-align:center;margin:30px 0"><a href="{{ $url }}" class="button" style="color: #fff !important">Reset Password</a></p>
 
       <p class="muted">This password reset link will expire in 60 minutes.</p>
       <p class="muted">If you did not request a password reset, no further action is required.</p>
