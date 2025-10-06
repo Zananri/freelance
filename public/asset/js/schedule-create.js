81df80f36759a2ca9678e0f116dc9b98e97fa88e
Syamil Maranri <symlmrnri17@gmail.com>
2025-10-06 15:33:28 +0700

    Update

--- public/asset/js/schedule-create.js
+++ public/asset/js/schedule-create.js
@@ -542,8 +542,7 @@ document.addEventListener('DOMContentLoaded', () => {
             .then((res) => res.json())
             .then((payload) => {
                 projects = (payload.data || [])
-                    .filter(p => !p.project_type || String(p.project_type) === 'public')
-                    .map((p) => ({
+                    .map(p => ({
                         id: p.id,
                         title: p.title,
                         image: p.image || "",
