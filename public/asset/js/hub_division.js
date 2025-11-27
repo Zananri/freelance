fa10ea047b8903a8527c1ec4965d7f250a33e8a0
Syamil Maranri <symlmrnri17@gmail.com>
2025-11-27 09:20:43 +0700

    Update animation loading in hub division page

--- public/asset/js/hub_division.js
+++ public/asset/js/hub_division.js
@@ -252,6 +252,9 @@ async function renderCalendar(year, month) {
 
 
     calendarBody.append(row);
+    
+    // Pastikan tabel kalender terlihat setelah rendering
+    $('.box-table-calendar').show();
 
     return 'done-rendering';
 }
@@ -314,17 +317,20 @@ $(document).on('click', '.month-item', function () {
 
 function showCalendarLoading() {
     $('.box-loader').removeClass('d-none');
+    $('.box-table-calendar').hide();
 }
 
 function hideCalendarLoading() {
     $('.box-loader').addClass('d-none');
+    $('.box-table-calendar').show();
 }
 
 async function renderEventCalendar(year, month) {
     try {
-        await renderCalendar(year, month);
-
+        // Tampilkan loading dan sembunyikan tabel terlebih dahulu
         showCalendarLoading();
+        
+        await renderCalendar(year, month);
 
         if (selectedEmployeeId) {
             const result = await loadEmployeeTasks(
@@ -954,6 +960,11 @@ $(document).ready(function () {
 
     $('.calendar-month').text(monthNames[currentDate.getMonth()]);
     $('.calendar-year').text(currentDate.getFullYear());
+    
+    // Pastikan tabel kalender dan loading dalam keadaan benar di awal
+    $('.box-table-calendar').show();
+    $('.box-loader').addClass('d-none');
+    
     renderCalendar(currentDate.getFullYear(), currentDate.getMonth());
 
     $('.selected-employee-photo').css('background-image', '');
