f76d9b55852bb58543cfd861efc8e7da6e4e7138
FahrelRhz <fahrelrhozian09@gmail.com>
2026-08-26 13:10:41 +0700

    update

--- resources/views/layouts/layout-guest.blade.php
+++ resources/views/layouts/layout-guest.blade.php
@@ -1,40 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Login Office</title>
 
     <meta name="description" content="Login Office">
     <meta name="keywords" content="sgs, SGS">
     <meta name="author" content="sgs.id">
-    <meta name="robots" content="index, nofollow"> 
+    <meta name="robots" content="index, nofollow">
     <meta name="theme-color" content="#0d6efd">
+
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="default">
     <meta name="apple-mobile-web-app-title" content="SGS HRIS">
 
-    <link rel="manifest" href="/manifest.webmanifest">
-    <link rel="apple-touch-icon" href="/asset/img/logo/logo.png">
+    <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
+    <link rel="apple-touch-icon" href="{{ asset('asset/img/logo/logo.png') }}">
     <link rel="icon" href="{{ asset('asset/img/favicon.ico') }}" type="image/x-icon">
     <link rel="shortcut icon" href="{{ asset('asset/img/favicon.ico') }}" type="image/x-icon">
 
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    
-    <link href="{{ asset('asset/css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('asset/css/guest.css') }}" rel="stylesheet">
- 
-     
+    <link
+        rel="preconnect"
+        href="https://fonts.googleapis.com"
+    >
+    <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin
+    >
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet"
+    >
+
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous"
+    >
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        rel="stylesheet"
+    >
+
+    <link
+        href="{{ asset('asset/css/app.css') }}"
+        rel="stylesheet"
+    >
+
+    <link
+        href="{{ asset('asset/css/guest.css') }}"
+        rel="stylesheet"
+    >
+
     <style>
-        body{
-            background-image: url('{{ asset('asset/img/background/light-1.jpg') }}')
+        body {
+            background-image: url('{{ asset('asset/img/background/light-1.jpg') }}');
         }
     </style>
 </head>
+
 <body>
     {{ $slot }}
 </body>
